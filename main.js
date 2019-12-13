e6a285310e9481b5a173b8fcf8a8fe873532b16a
KrimsN <deni.tim99@yandex.ru>
2019-12-13 18:26:51 +0700

    1

---
+++ main.js
@@ -0,0 +1,7 @@
+function translate() {
+    text_to_translate = document.getElementById('input_box').value;
+    ch_yandex = document.getElementById('ch_yandex').value;
+    ch_google = document.getElementById('ch_google').value;
+    
+
+}
